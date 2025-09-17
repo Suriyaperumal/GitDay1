f221cd20e21b17f5a4c7b49f0fa988c528833dcf
Suriyaperumal <Suriyaperumalspln@gmail.com>
2025-09-17 11:35:55 +0530

    new task

---
+++ GitDemo.js
@@ -0,0 +1,7 @@
+function details(name,age,location){
+    
+  console.log("The name is"+name)
+    console.log("The age is"+age)
+      console.log("The location is"+location)
+}
+details("Perumal",27,"Chennai")
